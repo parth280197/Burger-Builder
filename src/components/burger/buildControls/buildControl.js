547a56946c4cf02811ce2a9bbfa0940dc5289809
Parth <parth280197@gmail.com>
2019-09-11 12:51:45 -0500

    Order now button added

--- src/components/burger/buildControls/buildControl.js
+++ src/components/burger/buildControls/buildControl.js
@@ -6,18 +6,18 @@ const buildControl = props => {
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
       <button
-        className={classes.More}
-        onClick={props.addIngredient}
-        disabled={props.disableMore}
-      >
-        More
-      </button>
-      <button
         className={classes.Less}
         onClick={props.removeIngredient}
         disabled={props.disableLess}
       >
         Less
+      </button>
+      <button
+        className={classes.More}
+        onClick={props.addIngredient}
+        disabled={props.disableMore}
+      >
+        More
       </button>
     </div>
   );
--- src/components/burger/buildControls/buildControls.js
+++ src/components/burger/buildControls/buildControls.js
@@ -26,6 +26,7 @@ const buildControls = props => {
           />
         );
       })}
+      <button className={classes.OrderButton}>ORDER NOW</button>
     </div>
   );
 };
